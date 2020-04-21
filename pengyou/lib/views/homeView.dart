7358ab5e6365b41c5c65f6cd38db05c90844d038
Felipe Peter <mr-peipei@web.de>
2020-04-21 19:44:54 +0800

    Setup bottom navigation

--- pengyou/lib/views/homeView.dart
+++ pengyou/lib/views/homeView.dart
@@ -1,22 +1,46 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:pengyou/icons/custom_icons_icons.dart';
+import 'package:pengyou/values/strings.dart';
 
 class HomeView extends StatefulWidget {
+  final String title;
 
-  
+  HomeView({this.title});
 
   @override
   HomeViewState createState() => HomeViewState();
 }
 
 class HomeViewState extends State<HomeView> {
+  int _selectedIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: widget.title,
+        title: Text(widget.title),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(CustomIcons.ic_dictionary),
+            title: Text(AppStrings.dictionary),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CustomIcons.ic_settings),
+            title: Text(AppStrings.settings),
+          )
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
       ),
     );
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
   }
 }
